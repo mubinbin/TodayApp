859ea836adb8960551bc252d457e0a7326fab479
mubinbin <dinobunmu@gmail.com>
2024-09-28 13:13:49 -0500

    Request access to reminder data

--- Today/Models/ReminderStore.swift
+++ Today/Models/ReminderStore.swift
@@ -18,6 +18,25 @@ final class ReminderStore {
         EKEventStore.authorizationStatus(for: .reminder) == .fullAccess
     }
     
+    func requestAccess() async throws {
+        let status = EKEventStore.authorizationStatus(for: .reminder)
+        switch status {
+        case .fullAccess:
+            return
+        case .restricted:
+            throw TodayError.accessRestricted
+        case .notDetermined:
+            let accessGranted = try await ekStore.requestFullAccessToReminders()
+            guard accessGranted else {
+                throw TodayError.accessDenied
+            }
+        case .denied:
+            throw TodayError.accessDenied
+        @unknown default:
+            throw TodayError.unknown
+        }
+    }
+    
     func readAll() async throws -> [Reminder] {
         guard isAvailable else {
             throw TodayError.accessDenied
--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -10,17 +10,23 @@ import Foundation
 enum TodayError: LocalizedError {
     
     case accessDenied
+    case accessRestricted
     case failedReadingReminders
     case reminderHasNoDueData
+    case unknown
     
     var errorDescription: String? {
         switch self {
         case .accessDenied:
             return NSLocalizedString("The app doesn't have permission to read reminders.", comment: "access denied error description")
+        case .accessRestricted:
+            return NSLocalizedString("This device doesn't allow access to reminders", comment: "access restricted error description")
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders", comment: "failed reading reminders error description")
         case .reminderHasNoDueData:
             return NSLocalizedString("A reminder has no due data", comment: "reminder has no due data error description")
+        case .unknown:
+            return NSLocalizedString("An unknown error occurred", comment: "unknown error description")
         }
     }
 }
