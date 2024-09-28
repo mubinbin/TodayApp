e0fd9773b762b50d5c30633240f2322a3f0260d0
mubinbin <dinobunmu@gmail.com>
2024-09-28 12:30:20 -0500

    Convert between model types

---
+++ Today/Models/Reminder+EKReminder.swift
@@ -0,0 +1,22 @@
+//
+//  Reminder+EKReminder.swift
+//  Today
+//
+//  Created by Binbin Mu on 9/28/24.
+//
+
+import EventKit
+import Foundation
+
+extension Reminder {
+    init(with ekReminder: EKReminder) throws {
+        guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
+            throw TodayError.reminderHasNoDueData
+        }
+        id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
+    }
+}
--- Today/Models/TodayError.swift
+++ Today/Models/TodayError.swift
@@ -10,11 +10,14 @@ import Foundation
 enum TodayError: LocalizedError {
     
     case failedReadingReminders
+    case reminderHasNoDueData
     
     var errorDescription: String? {
         switch self {
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders", comment: "failed reading reminders error description")
+        case .reminderHasNoDueData:
+            return NSLocalizedString("A reminder has no due data", comment: "reminder has no due data error description")
         }
     }
 }
